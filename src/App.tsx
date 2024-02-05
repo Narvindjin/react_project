c2af644bca890336c2b502917a1e3e726151a631
Narvindjin <Narvindjin@gmail.com>
2024-02-05 13:38:14 +0500

    added swiper

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Wrapper from "./wrapper";
 import Header from "./blocks/header/header";
 import FrontMain from "./blocks/frontpage/front-main";
+import { register } from 'swiper/element/bundle';
 import CatalogueMain from "./blocks/catalogue/catalogue-main";
 import Footer from "./blocks/footer/footer";
 
 function App() {
+    register();
   return (
           <Wrapper>
               <Header/>
